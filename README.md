c29012f4b60a1cc6237c91a14fe3c48db533209a
Veskfall <124801297+Veskfall@users.noreply.github.com>
2024-08-02 12:10:44 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# website
+# Veskfall Games
+This is a tool to package, build and run [Veskfall](https://veskfall.itch.io/) apps, games for the web.
 
-[Veskfall Website](https://veskfall.github.io/website/)
+You can find it at https://veskfall.github.io/website/
+
+Link's List:
+
+[Veskfall Games](https://veskfall.github.io/website/)
+[VOID Fantasy Console](veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440000)
+[VOID Fantasy Console](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440001)
+[VOID Fantasy Console](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440002)
+[VOID Fantasy Console](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440003)
+[VOID Fantasy Console](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440004)
+[Veskfall BBS](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440005)
+[Veskfall BBS](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440006)
+[Veskfall BBS](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440007)
+[Veskfall IperBlog](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440008)
+[Veskfall Games:Info](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440009)
+[Contact](mailto:hey@veskfall.com)
+[Veskfall Games::Games](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440010)
+[Terms of Use](https://veskfall.github.io/website/550e8400-e29b-41d4-a716-446655440011)
--- script.js
+++ script.js
@@ -1,6 +1,53 @@
-function openDrawer() {
-  document.getElementById("drawer").style.display = "block";
+function loadPage(name) {
+  switch (name) {
+    case "homes":
+      window.location.href = "./";
+      break;
+    case "blog":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440000";
+      break;
+    case "intro":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440001";
+      break;
+    case "win":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440002";
+      break;
+    case "void-schools":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440003";
+      break;
+    case "void-submit":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440004";
+      break;
+    case "bbs":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440005";
+      break;
+    case "bbs-void":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440006";
+      break;
+    case "bbs-carts":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440007";
+      break;
+    case "bbs-blog":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440008";
+      break;
+    case "info":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440009";
+      break;
+    case "games":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440010";
+      break;
+    case "info-tos":
+      window.location.href = "./550e8400-e29b-41d4-a716-446655440011";
+      break;
+    default:
+      window.location.href = "./404";
+  }
 }
-function closeDrawer() {
-  document.getElementById("drawer").style.display = "none";
-}
+
+window.addEventListener("load", function() {
+  if (window.location.href.includes("550e8400-e29b-41d4-a716-446655440000")) {
+    document.getElementById("home").style.display = "block";
+  } else {
+    document.getElementById("home").style.display = "none";
+  }
+});
